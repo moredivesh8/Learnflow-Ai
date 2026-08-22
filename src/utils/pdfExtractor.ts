52317f634d50efeb27f7b784d8c3de9533761eb7
moredivesh8 <moredivesh8@gmail.com>
2026-08-22 18:19:11 +0530

    Update pdfExtractor.ts

--- src/utils/pdfExtractor.ts
+++ src/utils/pdfExtractor.ts
@@ -1,4 +1,7 @@
 import * as pdfjsLib from 'pdfjs-dist';
+import workerSrc from 'pdfjs-dist/build/pdf.worker.min.mjs?url';
+
+pdfjsLib.GlobalWorkerOptions.workerSrc = workerSrc;
 
 export interface ExtractedPdfResult {
   text: string;
